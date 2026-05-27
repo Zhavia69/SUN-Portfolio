8eac43c962ee589ad1bf0705768d7f581a7adf24
Zhavia69 <sammuelryan4050@gmail.com>
2026-05-27 12:37:27 +0300

    best

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,22 +5,17 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // GitHub Pages base path
-  base: "/SUN-Portfolio/",
-
-  // Build configuration
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
   build: {
-    // Changes output directory from dist to build
     outDir: "build",
     chunkSizeWarningLimit: 2000,
   },
-
   plugins: [tsconfigPaths(), react(), tagger()],
-
   server: {
-    port: 4028,
+    port: "4028",
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: [".amazonaws.com", ".builtwithrocket.new"],
-  },
+    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
+  }
 });
